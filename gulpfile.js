3640dcbb20e4f821ee6679487e2c39aea05db9ca
adrianmartinsen <ad.martinsen@gmail.com>
2020-12-24 21:44:05 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp'),
+      sass = require('gulp-dart-sass'),
+      browserSync = require('browser-sync').create();
+
+function style() {
+   return gulp.src('./scss/**/*.scss')
+   .pipe(sass().on('error', sass.logError))
+   .pipe(gulp.dest('./css'))
+   .pipe(browserSync.stream());
+}
+
+function watch() {
+   browserSync.init({
+      server: { baseDir: './'}
+   });
+   gulp.watch('./scss/**/*.scss', style);
+   gulp.watch('./*.html').on('change', browserSync.reload);
+   gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
